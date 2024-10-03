f395039d26cb13de093517e1384b1b5e88eb7b4d
Максим <maximers6e@gmail.com>
2024-10-03 10:11:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
-x = 2  
-y = 3 
+x = 2 # 1 переменная
+y = 3 # 2 переменная
 numerator = x**14 + math.exp(y - 1)
 denominator = 1 + x**y
 root_part = (abs(y - x)**2 / 2) + abs(y - x)**3
 h = (numerator / denominator) * root_part**(1/3)
-print(f"Значение h: {h}")
+print(f"Значение h: {h}") # ВЫВОД РЕЗУЛЬТАТА
